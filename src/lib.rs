46449669b63b2e0ada100f38b858f8a522bdc87e
Luis Tinajero Muñozcano <ltm150895@gmail.com>
2024-06-14 12:53:39 -0600

    add readme and create a new constructor

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,27 @@
 use crate::r#struct::env_config::EnvConfig;
 use crate::r#struct::log_level::LogLevel;
-use crate::r#struct::log_schema::LogSchema;
-
+// re-export LogSchema to make usable by consumer
+pub use crate::r#struct::log_schema::LogSchema;
 mod http_client;
 mod r#struct;
 
-struct Logger {
+pub struct Logger {
     env_config: EnvConfig,
 }
 
 impl Default for Logger {
     fn default() -> Self {
         let env_config = EnvConfig::default();
-        Logger { env_config }
+        Self { env_config }
     }
 }
 
 impl Logger {
+    pub fn new(app_version: String, verbose: bool) -> Self {
+        let env_config = EnvConfig::new(app_version, verbose);
+        Self { env_config }
+    }
+
     #[allow(dead_code)]
     pub async fn info(&self, log: LogSchema) {
         let env_config = &self.env_config;
@@ -49,20 +54,5 @@ impl Logger {
     pub async fn debug(&self, log: LogSchema) {
         let better_log = log.to_betterstack(&self.env_config, LogLevel::Debug);
         println!("{}", better_log);
-    }
-}
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
